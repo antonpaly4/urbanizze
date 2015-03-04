9246165c8de354ad13a58a5a8ad107e0397a13fd
antonpaly4 <anton.korchagin@takestep.ru>
2015-03-04 22:40:27 +0300

    viewport

---
+++ js/script.js
@@ -0,0 +1,18 @@
+(function(window, $){
+
+  var timeout = null;
+
+  $('.card_4').on('mouseover', function(){
+    var $card = $(this);
+    clearTimeout(timeout);
+    timeout = setTimeout(function(){
+      $('.description', $card).css('top', 0);
+      $('.description_holder', $card).css('opacity', 1);
+    }, 1000);
+  }).on('mouseleave', function(){
+    clearTimeout(timeout);
+    $('.description', $(this)).css('top', '100%');
+    $('.description_holder', $(this)).css('opacity', 0);
+  })
+
+})(window, jQuery);
