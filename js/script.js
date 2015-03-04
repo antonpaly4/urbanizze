7e0894cfc6f2fa33f2626d7a7f76c6c21ae1a054
antonpaly4 <anton.korchagin@takestep.ru>
2015-03-05 01:17:33 +0300

    improvements

--- js/script.js
+++ js/script.js
@@ -7,12 +7,10 @@
     clearTimeout(timeout);
     timeout = setTimeout(function(){
       $('.description', $card).css('top', 0);
-      $('.description_holder', $card).css('opacity', 1);
     }, 1000);
   }).on('mouseleave', function(){
     clearTimeout(timeout);
     $('.description', $(this)).css('top', '100%');
-    $('.description_holder', $(this)).css('opacity', 0);
   })
 
 })(window, jQuery);
